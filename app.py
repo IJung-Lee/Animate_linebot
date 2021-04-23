b8d1043ca8f45b07d79b422cf92b19b65b3bf828
IJung-Lee <eva88827@gmail.com>
2021-04-24 01:24:38 +0800

    2021.04.24 linebot

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import re
 from datetime import datetime
 
 from Msg_template import Ani_info
-# from Msg_template import Msg_test
+from Msg_template import Msg_quick
 from Msg_template import Msg_Template
 
 
@@ -48,13 +48,12 @@ def handle_message(event):
     if re.match("#", msg):
         search_result = Ani_info.ani_search(msg[1:])
         if len(search_result) > 1:
-            line_bot_api.push_message(uid, TextSendMessage(str(search_result[0])))
-            content = Msg_test.ani_name_select(search_result)
+            content = Msg_quick.ani_name_select(search_result)
             line_bot_api.push_message(uid, content)
         elif len(search_result) == 1:
             line_bot_api.push_message(uid, TextSendMessage(search_result[0]))
-            content = Msg_test.ani_information(search_result[0])
-            line_bot_api.push_message(uid, content)
+            # content = Msg_test.ani_information(search_result[0])
+            # line_bot_api.push_message(uid, content)
         else:
             line_bot_api.push_message(uid, TextSendMessage('查無此番劇，請重新搜尋。'))
     
