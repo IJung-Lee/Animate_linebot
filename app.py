1ca16cf4458090aebfed5a680ecae9eab871d209
IJung-Lee <eva88827@gmail.com>
2021-04-23 16:09:37 +0800

    2021.04.23 ver.4

--- app.py
+++ app.py
@@ -2,17 +2,18 @@ import re
 import os
 # from pymongo import MongoClient
 # import pymongo
+from datetime import datetime
 from flask import Flask, request, abort
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import MessageEvent, TextMessage, TextSendMessage
-# from linebot.models import *
+from linebot.models import *
 
-# import Msg_Ani
+import Msg_Ani
 from Msg_template import week_menu
 from Msg_Template import category_menu
 
-import Ani_info
+# import Ani_info
 
 app = Flask(__name__)
 
@@ -36,7 +37,6 @@ def callback():
     try:
         handler.handle(body, signature)
     except InvalidSignatureError:
-        print("Invalid signature. Please check your channel access token/channel secret.")
         abort(400)
 
     return 'OK'
@@ -49,26 +49,24 @@ def handle_message(event):
     profile = line_bot_api.get_profile(event.source.user_id) #獲取使用者資訊
     uid = profile.user_id # 使用者ID
 
-    line_bot_api.reply_message(
-        event.reply_token, TextSendMessage(text=f"Hello {line_bot_api.get_profile(event.source.user_id).display_name}!")
-    )
-    
-    # #星期
-    # if re.match("番劇時間", msg):
-    #     line_bot_api.push_message(uid, content)
-    #     content = Msg_Template.week_menu()
-    #     line_bot_api.push_message(uid, content)
-    #     return 0
-    # #類別
-    # elif re.match("番劇類別", msg):
-    #     content = Msg_Template.category_menu()
-    #     line_bot_api.push_message(uid, content)
-    #     return 0
+    #星期
+    if re.match("番劇時間", msg):
+        line_bot_api.push_message(uid, content)
+        content = Msg_Template.week_menu()
+        line_bot_api.push_message(uid, content)
+        return 0
+    #類別
+    elif re.match("番劇類別", msg):
+        content = Msg_Template.category_menu()
+        line_bot_api.push_message(uid, content)
+        return 0
 
-    # elif re.match("哈囉", msg):
-    #     line_bot_api.push_message(uid, TextSendMessage('你好啊'))
-    #     return 0
+    elif re.match("哈囉", msg):
+        line_bot_api.push_message(uid, TextSendMessage('你好啊'))
+        return 0
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
+    # app.run(debug=True)
