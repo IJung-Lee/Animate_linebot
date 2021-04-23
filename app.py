9d6fb98262cda0c6f990152fb9d07a10a3502d35
IJung-Lee <eva88827@gmail.com>
2021-04-23 18:13:17 +0800

    2021.04.23 linebot

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import os
 import re
 from datetime import datetime
 
-from Msg_template import Msg_Template
+# from Msg_template import Msg_Template
 
 from flask import Flask, abort, request
 from linebot import LineBotApi, WebhookHandler
@@ -35,6 +35,131 @@ def callback():
     return 'OK'
 
 
+def week_menu(): 
+    flex_message = FlexSendMessage(
+            alt_text = "Week Menu",
+            contents = {
+                "type": "bubble",
+                "hero": {
+                    "type": "image",
+                    "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_1_cafe.png",
+                    "size": "full",
+                    "aspectRatio": "5:2",
+                    "aspectMode": "cover"
+                },
+                "body": {
+                    "type": "box",
+                    "layout": "vertical",
+                    "contents": [
+                    {
+                        "type": "text",
+                        "text": "番劇時間",
+                        "weight": "bold",
+                        "size": "xl",
+                        "align": "center"
+                    },
+                    {
+                        "type": "box",
+                        "layout": "vertical",
+                        "spacing": "sm",
+                        "contents": [
+                        {
+                            "type": "box",
+                            "layout": "horizontal",
+                            "contents": [
+                            {
+                                "type": "button",
+                                "action": {
+                                "type": "message",
+                                "label": "週一",
+                                "text": "星期一番劇查詢"
+                                },
+                                "height": "sm",
+                                "style": "link"
+                            },
+                            {
+                                "type": "button",
+                                "action": {
+                                "type": "message",
+                                "label": "週二",
+                                "text": "星期二番劇查詢"
+                                },
+                                "height": "sm",
+                                "style": "link"
+                            },
+                            {
+                                "type": "button",
+                                "action": {
+                                "type": "message",
+                                "label": "週三",
+                                "text": "星期三番劇查詢"
+                                },
+                                "height": "sm",
+                                "style": "link"
+                            }
+                            ],
+                            "paddingAll": "none"
+                        },
+                        {
+                            "type": "box",
+                            "layout": "horizontal",
+                            "contents": [
+                            {
+                                "type": "button",
+                                "action": {
+                                "type": "message",
+                                "label": "週四",
+                                "text": "星期四番劇查詢"
+                                },
+                                "height": "sm",
+                                "style": "link"
+                            },
+                            {
+                                "type": "button",
+                                "action": {
+                                "type": "message",
+                                "label": "週五",
+                                "text": "星期五番劇查詢"
+                                },
+                                "height": "sm",
+                                "style": "link"
+                            },
+                            {
+                                "type": "button",
+                                "action": {
+                                "type": "message",
+                                "label": "週六",
+                                "text": "星期六番劇查詢"
+                                },
+                                "height": "sm",
+                                "style": "link"
+                            },
+                            {
+                                "type": "button",
+                                "action": {
+                                "type": "message",
+                                "label": "週日",
+                                "text": "星期日番劇查詢"
+                                },
+                                "height": "sm",
+                                "style": "link"
+                            }
+                            ],
+                            "paddingAll": "none"
+                        },
+                        {
+                            "type": "spacer"
+                        }
+                        ],
+                        "paddingAll": "xs"
+                    }
+                    ],
+                    "paddingAll": "md"
+                }
+            }
+    )
+    return flex_message
+
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     msg = str(event.message.text).upper().strip() # 使用者輸入的內容
@@ -43,17 +168,15 @@ def handle_message(event):
 
     #時間
     if re.match("時間", msg):
-        flex_message = Msg_Template.week_menu()
+        flex_message = week_menu()
         line_bot_api.push_message(uid, flex_message)
-        return 0
 
     #類別
     elif re.match("類別", msg):
         flex_message = Msg_Template.category_menu()
         line_bot_api.push_message(uid, flex_message)
-        return 0
     else:
-        line_bot_api.push_message(uid, TextSendMessage('很抱歉我們無法回應該訊息 \n\n輸入《時間》查找各類番劇！ \n輸入《類別》找尋每日番劇！'))
+        line_bot_api.push_message(uid, TextSendMessage('很抱歉我們無法回應該訊息 \n\n輸入《時間》找尋每日番劇！ \n輸入《類別》查找各類番劇！'))
 
 
 
