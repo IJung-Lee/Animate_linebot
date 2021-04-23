cf18ef0f942674da833bdb535e0cd9b8fb1b496c
IJung-Lee <eva88827@gmail.com>
2021-04-23 15:25:24 +0800

    2021.04.23 ver.3

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from flask import Flask, request, abort
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import MessageEvent, TextMessage, TextSendMessage
-from linebot.models import *
+# from linebot.models import *
 
 # import Msg_Ani
 from Msg_template import week_menu
@@ -17,8 +17,10 @@ import Ani_info
 app = Flask(__name__)
 
 
-line_bot_api = LineBotApi(os.environ("CHANNEL_ACCESS_TOKEN"))
-handler = WebhookHandler(os.environ("CHANNEL_SECRET"))
+# line_bot_api = LineBotApi(os.environ("CHANNEL_ACCESS_TOKEN"))
+# handler = WebhookHandler(os.environ("CHANNEL_SECRET"))
+line_bot_api = LineBotApi(os.environ['CHANNEL_ACCESS_TOKEN'])
+handler = WebhookHandler(os.environ['CHANNEL_SECRET'])
 
 # 監聽所有來自 /callback 的 Post Request
 @app.route("/callback", methods=['POST'])
@@ -47,22 +49,25 @@ def handle_message(event):
     profile = line_bot_api.get_profile(event.source.user_id) #獲取使用者資訊
     uid = profile.user_id # 使用者ID
 
+    line_bot_api.reply_message(
+        event.reply_token, TextSendMessage(text=f"Hello {line_bot_api.get_profile(event.source.user_id).display_name}!")
+    )
+    
+    # #星期
+    # if re.match("番劇時間", msg):
+    #     line_bot_api.push_message(uid, content)
+    #     content = Msg_Template.week_menu()
+    #     line_bot_api.push_message(uid, content)
+    #     return 0
+    # #類別
+    # elif re.match("番劇類別", msg):
+    #     content = Msg_Template.category_menu()
+    #     line_bot_api.push_message(uid, content)
+    #     return 0
 
-    #星期
-    if re.match("番劇時間", msg):
-        line_bot_api.push_message(uid, content)
-        content = Msg_Template.week_menu()
-        line_bot_api.push_message(uid, content)
-        return 0
-    #類別
-    elif re.match("番劇類別", msg):
-        content = Msg_Template.category_menu()
-        line_bot_api.push_message(uid, content)
-        return 0
-
-    elif re.match("哈囉", msg):
-        line_bot_api.push_message(uid, TextSendMessage('你好啊'))
-        return 0
+    # elif re.match("哈囉", msg):
+    #     line_bot_api.push_message(uid, TextSendMessage('你好啊'))
+    #     return 0
 
 
 if __name__ == "__main__":
