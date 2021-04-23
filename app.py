257e55d41fb1e1f39e429c0fd2a166f9d327eba9
IJung-Lee <eva88827@gmail.com>
2021-04-23 23:38:56 +0800

    2021.04.23 linebot

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import os
 import re
 from datetime import datetime
 
-# from Msg_template import Ani_info
+import Ani_info
 # from Msg_template import Msg_test
 from Msg_template import Msg_Template
 
