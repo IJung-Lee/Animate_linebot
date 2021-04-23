ece518c4bcf9c46f9c220ceceb11a09c7c41c885
IJung-Lee <eva88827@gmail.com>
2021-04-23 17:12:15 +0800

    2021.04.23 app, msg

--- app.py
+++ app.py
@@ -3,10 +3,12 @@ import re
 from datetime import datetime
 
 from flask import Flask, abort, request
-
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import MessageEvent, TextMessage, TextSendMessage
+
+from Msg_template import Msg_Ani
+from Msg_template import Msg_Template
 
 app = Flask(__name__)
 
@@ -39,8 +41,16 @@ def handle_message(event):
     profile = line_bot_api.get_profile(event.source.user_id)
     uid = profile.user_id # 發訊者ID
 
-    if re.match("哈哈", msg):
-        line_bot_api.push_message(uid, TextSendMessage('啊這'))
+    #時間
+    if re.match("時間", msg):
+        content = Msg_Template.week_menu()
+        line_bot_api.push_message(uid, content)
+
+    #類別
+    elif re.match("類別", msg):
+        content = Msg_Template.category_menu()
+        line_bot_api.push_message(uid, content)
+
     else:
         line_bot_api.push_message(uid, TextSendMessage('我不懂你在說啥'))
 
