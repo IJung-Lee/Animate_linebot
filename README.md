295102b01cb5db1397109c453396f431ef946cf5
IJung-Lee <eva88827@gmail.com>
2021-04-23 14:45:43 +0800

    2021.04.23 ver.1

--- README.md
+++ README.md
@@ -1,7 +1,2 @@
 # Animation_linebot
 The project for Linebot.
-
-<a href="https://heroku.com/deploy?template=https://github.com/IJung-Lee/Animation_linebot/tree/main">
-  <img src="https://www.herokucdn.com/deploy/button.svg" alt="Deploy">
-</a>
-
--- app.py
+++ app.py
@@ -21,24 +21,40 @@ app = Flask(__name__)
 line_bot_api = LineBotApi(os.environ.get("CHANNEL_ACCESS_TOKEN"))
 handler = WebhookHandler(os.environ.get("CHANNEL_SECRET"))
 
-# 監聽所有來自 /callback 的 Post Request
-@app.route("/callback", methods=['POST'])
+@app.route("/", methods=["GET", "POST"])
 def callback():
-    # get X-Line-Signature header value
-    signature = request.headers['X-Line-Signature']
 
-    # get request body as text
-    body = request.get_data(as_text=True)
-    app.logger.info("Request body: " + body)
+    if request.method == "GET":
+        return "Hello Heroku"
+    if request.method == "POST":
+        signature = request.headers["X-Line-Signature"]
+        body = request.get_data(as_text=True)
 
-    # handle webhook body
-    try:
-        handler.handle(body, signature)
-    except InvalidSignatureError:
-        print("Invalid signature. Please check your channel access token/channel secret.")
-        abort(400)
+        try:
+            handler.handle(body, signature)
+        except InvalidSignatureError:
+            abort(400)
 
-    return 'OK'
+        return "OK"
+
+# # 監聽所有來自 /callback 的 Post Request
+# @app.route("/callback", methods=['POST'])
+# def callback():
+#     # get X-Line-Signature header value
+#     signature = request.headers['X-Line-Signature']
+
+#     # get request body as text
+#     body = request.get_data(as_text=True)
+#     app.logger.info("Request body: " + body)
+
+#     # handle webhook body
+#     try:
+#         handler.handle(body, signature)
+#     except InvalidSignatureError:
+#         print("Invalid signature. Please check your channel access token/channel secret.")
+#         abort(400)
+
+#     return 'OK'
 
 
 # 處理訊息
@@ -48,15 +64,15 @@ def handle_message(event):
     profile = line_bot_api.get_profile(event.source.user_id) #獲取使用者資訊
     uid = profile.user_id # 使用者ID
 
+
     #星期
     if re.match("番劇時間", msg):
-        content = questionnaire.Q1_menu()
+        content = Msg_Template.week_menu()
         line_bot_api.push_message(uid, content)
         return 0
-
     #類別
     elif re.match("番劇類別", msg):
-        content = questionnaire.Q1_menu()
+        content = Msg_Template.category_menu()
         line_bot_api.push_message(uid, content)
         return 0
 
