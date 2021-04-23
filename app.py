02c906e36afcb146fc1c1ee7c061cfc5b27f6678
IJung-Lee <eva88827@gmail.com>
2021-04-23 15:14:02 +0800

    2021.04.23 ver.2

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-import re, os
+import re
+import os
 # from pymongo import MongoClient
 # import pymongo
 from flask import Flask, request, abort
@@ -15,46 +16,28 @@ import Ani_info
 
 app = Flask(__name__)
 
-# line_bot_api = LineBotApi('YOUR_CHANNEL_ACCESS_TOKEN')
-# handler = WebhookHandler('YOUR_CHANNEL_SECRET')
 
-line_bot_api = LineBotApi(os.environ.get("CHANNEL_ACCESS_TOKEN"))
-handler = WebhookHandler(os.environ.get("CHANNEL_SECRET"))
+line_bot_api = LineBotApi(os.environ("CHANNEL_ACCESS_TOKEN"))
+handler = WebhookHandler(os.environ("CHANNEL_SECRET"))
 
-@app.route("/", methods=["GET", "POST"])
+# 監聽所有來自 /callback 的 Post Request
+@app.route("/callback", methods=['POST'])
 def callback():
+    # get X-Line-Signature header values
+    signature = request.headers['X-Line-Signature']
 
-    if request.method == "GET":
-        return "Hello Heroku"
-    if request.method == "POST":
-        signature = request.headers["X-Line-Signature"]
-        body = request.get_data(as_text=True)
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
 
-        try:
-            handler.handle(body, signature)
-        except InvalidSignatureError:
-            abort(400)
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        print("Invalid signature. Please check your channel access token/channel secret.")
+        abort(400)
 
-        return "OK"
-
-# # 監聽所有來自 /callback 的 Post Request
-# @app.route("/callback", methods=['POST'])
-# def callback():
-#     # get X-Line-Signature header value
-#     signature = request.headers['X-Line-Signature']
-
-#     # get request body as text
-#     body = request.get_data(as_text=True)
-#     app.logger.info("Request body: " + body)
-
-#     # handle webhook body
-#     try:
-#         handler.handle(body, signature)
-#     except InvalidSignatureError:
-#         print("Invalid signature. Please check your channel access token/channel secret.")
-#         abort(400)
-
-#     return 'OK'
+    return 'OK'
 
 
 # 處理訊息
@@ -67,6 +50,7 @@ def handle_message(event):
 
     #星期
     if re.match("番劇時間", msg):
+        line_bot_api.push_message(uid, content)
         content = Msg_Template.week_menu()
         line_bot_api.push_message(uid, content)
         return 0
@@ -74,6 +58,10 @@ def handle_message(event):
     elif re.match("番劇類別", msg):
         content = Msg_Template.category_menu()
         line_bot_api.push_message(uid, content)
+        return 0
+
+    elif re.match("哈囉", msg):
+        line_bot_api.push_message(uid, TextSendMessage('你好啊'))
         return 0
 
 
